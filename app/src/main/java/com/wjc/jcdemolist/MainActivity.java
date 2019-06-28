dc6dc9af0a34912cf24b0d83fbc57f1c11bf73a4
chenwangjun <wjchenpl@foxmail.com>
2019-06-28 18:13:11 +0800

    Initial commit

---
+++ app/src/main/java/com/wjc/jcdemolist/MainActivity.java
@@ -0,0 +1,30 @@
+package com.wjc.jcdemolist;
+
+import android.os.Bundle;
+
+import com.wjc.jcdemolist.base.BaseActivity;
+import com.wjc.jcdemolist.bean.ActivityTypeBean;
+import com.wjc.jcdemolist.itemActivity.MarqueeActivity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public int setContentLayoutId() {
+        return R.layout.activity_main;
+    }
+
+    @Override
+    public List<ActivityTypeBean> getList() {
+        List<ActivityTypeBean> list = new ArrayList<>();
+        list.add(new ActivityTypeBean("跑马灯测试", MarqueeActivity.class));
+        return list;
+    }
+}
---
+++ app/src/main/java/com/wjc/jcdemolist/base/BaseActivity.java
@@ -0,0 +1,63 @@
+package com.wjc.jcdemolist.base;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.TextView;
+
+import com.wjc.jcdemolist.R;
+import com.wjc.jcdemolist.bean.ActivityTypeBean;
+
+import java.util.List;
+
+
+/**
+ * ClassName:com.example.wjc.myapplication
+ * Description:
+ * JcChen on 2019/6/16 10:16
+ */
+public abstract class BaseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(setContentLayoutId());
+        Adapter adapter = new Adapter(this);
+        adapter.setDates(getList());
+        RecyclerView recyclerView = findViewById(R.id.rl_base);
+        recyclerView.setLayoutManager(new GridLayoutManager(this,2));
+        recyclerView.setAdapter(adapter);
+    }
+
+    public abstract int setContentLayoutId();
+
+    public abstract List<ActivityTypeBean> getList();
+
+    private class Adapter extends BaseAdapter<ActivityTypeBean> {
+        public Adapter(Context context) {
+            super(context);
+        }
+
+        @Override
+        public int getLayoutId() {
+            return R.layout.item_base;
+        }
+
+        @Override
+        public void onBindItemHolder(BaseViewHolder holder, final int position) {
+            TextView testName = holder.getView(R.id.tv_test_name);
+            testName.setText(mDates.get(position).getName());
+            testName.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    context.startActivity(new Intent(context, mDates.get(position).getIntentClass()));
+                }
+            });
+        }
+    }
+}
