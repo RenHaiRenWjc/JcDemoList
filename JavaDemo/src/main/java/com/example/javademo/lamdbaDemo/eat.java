af43a5a983b01f0588dd597b04477ea2631f2e70
chenwangjun <wjchenpl@foxmail.com>
2019-07-04 08:43:29 +0800

    lamdba

---
+++ JavaDemo/src/main/java/com/example/javademo/lamdbaDemo/eat.java
@@ -0,0 +1,10 @@
+package com.example.javademo.lamdbaDemo;
+
+/**
+ * ClassName:com.example.javademo.lamdbaDemo
+ * Description:
+ * JcChen on 2019/7/4 8:36
+ */
+public interface eat<T> {
+    public void completed(T t1, T t2);
+}
