370fa020b408cc3a3cb061d23d7acdf622fde0cb
chenwangjun <wjchenpl@foxmail.com>
2019-10-14 23:42:47 +0800

    canvas 剪切

--- app/src/main/java/com/wjc/jcdemolist/demo/PaintAndCanvas/RoundViewFragment.java
+++ app/src/main/java/com/wjc/jcdemolist/demo/PaintAndCanvas/RoundViewFragment.java
@@ -9,16 +9,19 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.wjc.jcdemolist.R;
+import com.wjc.jcdemolist.demo.customView.CustomRoundView;
 
 /**
  * ClassName:com.wjc.jcdemolist.demo.PaintAndCanvas
- * Description: 圆形图片
+ * Description: 圆形图片  CustomRoundView
  * JcChen on 2019/10/2 9:28
  */
 public class RoundViewFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_roundview_layout, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_roundview_layout, container, false);
+        CustomRoundView customRoundView = rootView.findViewById(R.id.cv_test);
+        return rootView;
     }
 }
--- app/src/main/java/com/wjc/jcdemolist/demo/customView/CustomRoundView.java
+++ app/src/main/java/com/wjc/jcdemolist/demo/customView/CustomRoundView.java
@@ -3,6 +3,7 @@ package com.wjc.jcdemolist.demo.customView;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.BitmapShader;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
@@ -12,6 +13,7 @@ import android.graphics.PorterDuffXfermode;
 import android.graphics.Rect;
 import android.graphics.RectF;
 import android.graphics.Region;
+import android.graphics.Shader;
 import android.util.AttributeSet;
 import android.util.DisplayMetrics;
 import android.view.View;
@@ -70,23 +72,28 @@ public class CustomRoundView extends View {
         float x = 0, y = 0;
 
 //        canvas.drawBitmap(mBitmap, 0, 0, mPaint);
-        mBitmapEx = processRoundBitmap(mBitmap);
-        canvas.drawBitmap(mBitmapEx, x, y, mPaint);
+//        mBitmapEx = processRoundBitmap(mBitmap);
+//        canvas.drawBitmap(mBitmapEx, x, y, mPaint);
 
-//        canvas.save();
-//        mPaint.setAntiAlias(true);
-//        LogUtils.i(TAG, "onDraw: x=" + x + ",y=" + y + ",width=" + width + ",height=" + height);
-//        canvas.clipRect(x, y, x + width, y + height);  //用于裁剪画布，也就是设置画布的显示区域
-//        canvas.drawBitmap(mBitmap, x, y, mPaint);
-//
-////        // Rect 与 RectF 区别：http://teachcourse.cn/2268.html
-//        mClipPath.addOval(new RectF(x, y, x + width, y + width),
-//                Path.Direction.CCW);//圆，外切矩形
-//
-//        canvas.clipPath(mClipPath, Region.Op.DIFFERENCE); //是A形状中不同于B的部分显示出来
-//        canvas.drawColor(Color.WHITE);
-//        canvas.restore();
+//        processRoundBitmap02(canvas, x, y);
+        test(canvas, mBitmap, new RectF(0, 200, 200, 400));
+//        canvas.drawColor(Color.RED);
+    }
 
+    private void processRoundBitmap02(Canvas canvas, float x, float y) {
+        canvas.save();
+        mPaint.setAntiAlias(true);
+        LogUtils.i(TAG, "onDraw: x=" + x + ",y=" + y + ",width=" + width + ",height=" + height);
+        canvas.clipRect(x, y, x + width, y + height);  //用于裁剪画布，也就是设置画布的显示区域
+        canvas.drawBitmap(mBitmap, x, y, mPaint);
+
+//        // Rect 与 RectF 区别：http://teachcourse.cn/2268.html
+        mClipPath.addOval(new RectF(x, y, x + width, y + width),
+                Path.Direction.CCW);//圆，外切矩形
+
+        canvas.clipPath(mClipPath, Region.Op.DIFFERENCE); //是A形状中不同于B的部分显示出来
+//        canvas.drawColor(Color.TRANSPARENT);
+        canvas.restore();
     }
 
 
@@ -114,5 +121,20 @@ public class CustomRoundView extends View {
         canvas.drawBitmap(bitmap, 0, 0, paint);
 
         return bitmapEx;
+    }
+
+
+    private Bitmap test(Canvas canvas, Bitmap source, RectF rectF) {
+//        Canvas canvas = new Canvas();
+        canvas.drawColor(Color.RED);
+        Paint paint = new Paint();
+        paint.setAntiAlias(true);
+        paint.setShader(new BitmapShader(source, Shader.TileMode.CLAMP, Shader.TileMode.CLAMP));
+        LogUtils.i(TAG, "test: 11111111111");
+        Path path = new Path();
+        path.addCircle(rectF.right / 2, rectF.bottom - rectF.height() / 2, 100, Path.Direction.CCW);
+        canvas.clipPath(path);
+        canvas.drawBitmap(source, 0, 0, paint);
+        return source;
     }
 }
