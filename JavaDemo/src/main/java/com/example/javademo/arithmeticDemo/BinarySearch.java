c3b06f82fbf89803efcbf9ad354c4d6fb1249465
wjchenpl5tgb <wjchenpl@foxmail.com>
2019-10-17 10:35:54 +0800

    单链表 两个数相加

--- JavaDemo/src/main/java/com/example/javademo/arithmeticDemo/BinarySearch.java
+++ JavaDemo/src/main/java/com/example/javademo/arithmeticDemo/BinarySearch.java
@@ -9,7 +9,8 @@ public class BinarySearch {
     private static final int[] orderList = {1, 2, 3, 4, 5, 6, 7, 8, 9};
 
     public static void main(String[] args) {
-        System.out.println("binarySearch --:" + binarySearch(31));
+//        System.out.println("binarySearch --:" + binarySearch(31));
+        System.out.println("addTwoNumbers --:" + addTwoNumbers(null, null));
     }
 
     /**
@@ -30,5 +31,43 @@ public class BinarySearch {
             }
         }
         return -1;
+    }
+
+    // https://leetcode-cn.com/problems/add-two-numbers/comments/
+    public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        l1 = new ListNode(3);
+
+        l1.next = new ListNode(4);
+        l1.next.next = new ListNode(4);
+
+        l2 = new ListNode(4);
+        l2.next = new ListNode(6);
+        l2.next.next = new ListNode(1);
+
+        ListNode result = new ListNode(0);
+        ListNode nextResult = result;
+        int temp = 0;
+        while (l1 != null || l2 != null || temp != 0) {
+            int l1Val = l1 != null ? l1.val : 0;
+            int l2Val = l2 != null ? l2.val : 0;
+            int val = (l1Val + l2Val + temp) % 10;
+            temp = (l1Val + l2Val) / 10;
+            ListNode node = new ListNode(val);
+            nextResult.next = node; // result 的 next result.next = node
+            nextResult = node;  // nextResult 重置
+            if (l1 != null) l1 = l1.next;
+            if (l2 != null) l2 = l2.next;
+
+        }
+        return result.next;
+    }
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
     }
 }
