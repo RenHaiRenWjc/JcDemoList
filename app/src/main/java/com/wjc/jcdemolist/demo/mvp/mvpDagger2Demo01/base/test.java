77df4b1ef11c73adbc9a3efc519852e2d082113a
chenwangjun <wjchenpl@foxmail.com>
2019-11-03 10:18:59 +0800

    test

---
+++ app/src/main/java/com/wjc/jcdemolist/demo/mvp/mvpDagger2Demo01/base/test.java
@@ -0,0 +1,9 @@
+package com.wjc.jcdemolist.demo.mvp.mvpDagger2Demo01.base;
+
+/**
+ * ClassName:com.wjc.jcdemolist.demo.mvp.mvpDagger2Demo01.base
+ * Description:
+ * JcChen on 2019/11/3 10:16
+ */
+public class test {
+}
