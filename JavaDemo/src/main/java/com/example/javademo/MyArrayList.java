00d218914643a7a8367e60b7db50c3bfdec946b1
jcChen <wjchenrhr@qq.com>
2020-02-17 00:18:15 +0800

    自定义 ArrayList

---
+++ JavaDemo/src/main/java/com/example/javademo/MyArrayList.java
@@ -0,0 +1,119 @@
+package com.example.javademo;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+
+public class MyArrayList<E> {
+
+  private static final int DEFAULT_CAPACITY = 10;
+
+  private int theSize;
+  private E[] theItems;
+
+  public MyArrayList() {
+    doClear();
+  }
+
+  public boolean add(E e) {
+    add(size(), e);
+    return true;
+  }
+
+  public void add(int position, E e) {
+    if (theItems.length == size()) {
+      ensureCapacity(size() * 2 + 1);
+    }
+    for (int i = theSize; i > position; i--) {
+      theItems[i] = theItems[i - 1];
+    }
+    theItems[position] = e;
+    theSize++;
+  }
+
+  public E remove(int position) {
+    E removedItem = theItems[position];
+    for (int i = position; i < size() - 1; i++) {
+      theItems[i] = theItems[i + 1];
+    }
+    theSize--;
+    return removedItem;
+  }
+
+
+  public E get(int position) {
+    if (position < 0 || position >= size()) {
+      throw new ArrayIndexOutOfBoundsException();
+    }
+    return theItems[position];
+  }
+
+  public E set(int position, E newValue) {
+    if (position < 0 || position >= size()) {
+      throw new ArrayIndexOutOfBoundsException();
+    }
+    E old = theItems[position];
+    theItems[position] = newValue;
+    return old;
+  }
+
+  public void clear() {
+    doClear();
+  }
+
+
+  public void doClear() {
+    theSize = 0;
+    ensureCapacity(DEFAULT_CAPACITY);
+  }
+
+  // 容量扩充
+  private void ensureCapacity(int newCapacity) {
+    if (newCapacity < theSize) {
+      return;
+    }
+    E[] old = theItems;
+    theItems = (E[]) new Object[newCapacity];
+    for (int i = 0; i < size(); i++) {
+      theItems[i] = old[i];
+    }
+  }
+
+  public int size() {
+    return theSize;
+  }
+
+  public boolean isEmpty() {
+    return size() == 0;
+  }
+
+  public void trimToSize() {
+    ensureCapacity(size());
+  }
+
+  public java.util.Iterator<E> iterator() {
+    return new ArrayListIterator();
+  }
+
+  public class ArrayListIterator implements Iterator<E> {
+    private int current = 0;
+
+    @Override
+    public boolean hasNext() {
+      return current < size();
+    }
+
+    @Override
+    public E next() {
+      if (!hasNext()) throw new NoSuchElementException();
+      return theItems[current++];
+    }
+
+    @Override
+    public void remove() {
+      MyArrayList.this.remove(--current);
+    }
+  }
+
+
+}
--- JavaDemo/src/main/java/com/example/javademo/MyClass.java
+++ JavaDemo/src/main/java/com/example/javademo/MyClass.java
@@ -1,4 +1,39 @@
 package com.example.javademo;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 public class MyClass {
+
+
+  public static void main(String[] args) {
+   List list= new ArrayList<>();
+   list.add(1);
+   list.add(12);
+   list.add(13);
+    test03(list);
+  }
+
+  public static void test01(List<Integer> list){
+    for (int i=0;i<list.size();i++){
+      list.remove(i);
+      System.out.println("000");
+    }
+  }
+  public static void test02(List<Integer> list){
+    for (Integer i:list){
+      list.remove(i);
+      System.out.println("000");
+    }
+  }
+
+  public static void test03(List<Integer> list){
+    Iterator<Integer> iterable = list.iterator();
+    while (iterable.hasNext()){
+      iterable.next();
+      iterable.remove();
+      System.out.println("000");
+    }
+  }
 }
