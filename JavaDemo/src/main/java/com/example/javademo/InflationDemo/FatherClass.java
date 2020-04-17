414acb1af31f745fc5c04ca2d9b8171a9f76db5d
jcChen <wjchenrhr@qq.com>
2020-04-17 15:11:59 +0800

    反射相关

---
+++ JavaDemo/src/main/java/com/example/javademo/InflationDemo/FatherClass.java
@@ -0,0 +1,13 @@
+package com.example.javademo.InflationDemo;
+
+/**
+ * ClassName: com.example.javademo.InflationDemo
+ * Description:
+ * JcChen on 2020.04.07.21:54
+ */
+public class FatherClass {
+  public String mFatherName;
+  public int mFatherAge;
+
+  public void printFatherMsg(){}
+}
