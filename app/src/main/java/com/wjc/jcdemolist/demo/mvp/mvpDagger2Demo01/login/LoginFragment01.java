14d9f109ba3c4c1127b1f8d3107ec8dc0d4edbc1
jcChen <wjchenrhr@qq.com>
2020-05-29 16:13:59 +0800

    Firebase 邮箱登录

--- app/src/main/java/com/wjc/jcdemolist/demo/mvp/mvpDagger2Demo01/login/LoginFragment01.java
+++ app/src/main/java/com/wjc/jcdemolist/demo/mvp/mvpDagger2Demo01/login/LoginFragment01.java
@@ -1,9 +1,18 @@
 package com.wjc.jcdemolist.demo.mvp.mvpDagger2Demo01.login;
 
 
+import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.jakewharton.rxbinding3.view.RxView;
 import com.jakewharton.rxbinding3.widget.RxTextView;
 import com.wjc.jcdemolist.R;
@@ -23,73 +32,105 @@ import io.reactivex.android.schedulers.AndroidSchedulers;
  * JcChen on 2019/7/15 22:52
  */
 public class LoginFragment01 extends BaseFragment<LoginContract.Presenter> implements LoginContract.View {
-    private static final String TAG = "LoginFragment01";
-    @BindView(R.id.name)
-    EditText name;
-    @BindView(R.id.pwd)
-    EditText pwd;
-    @BindView(R.id.bt_ok)
-    Button btnLogin;
-//    Unbinder unbinder;
+	private static final String TAG = "LoginFragment01";
+	@BindView(R.id.name)
+	EditText name;
+	@BindView(R.id.pwd)
+	EditText pwd;
+	@BindView(R.id.bt_ok_test01)
+	Button btnLogin;
+	//    Unbinder unbinder;
+	private FirebaseAuth mAuth;
 
 
-    protected static LoginFragment01 newInstance() {
-        return new LoginFragment01();
-    }
+	protected static LoginFragment01 newInstance() {
+		return new LoginFragment01();
+	}
 
-    @Override
-    protected int layoutId() {
-        return R.layout.activity_login;
-    }
+	@Override
+	protected int layoutId() {
+		return R.layout.activity_login;
+	}
 
-    @Override
-    protected void initData() {
+	@Override
+	protected void initData() {
+		mAuth = FirebaseAuth.getInstance();
 
-    }
+	}
 
-    private static boolean isUsrValid(String usr) {
-        return true;
-    }
+	@Override
+	public void onResume() {
+		super.onResume();
+		FirebaseUser currentUser = mAuth.getCurrentUser();
+		if (currentUser == null) {//没有登录
 
-    private static boolean isPasswordValid(String pwd) {
-        return true;
-    }
+		} else {
+//			name.setText(currentUser.getPhoneNumber());
+//			pwd.setText(currentUser.getDisplayName());
+		}
+
+	}
+
+	private static boolean isUsrValid(String usr) {
+		return true;
+	}
+
+	private static boolean isPasswordValid(String pwd) {
+		return true;
+	}
 
 
-    @Override
-    public void loginSuccess(LoginContract.Model result) {
-        LogTools.i(TAG, "loginSuccess: result=" + result.getMsg() + ", is " + result.isSuccess());
-    }
+	@Override
+	public void loginSuccess(LoginContract.Model result) {
+		LogTools.i(TAG, "loginSuccess: result=" + result.getMsg() + ", is " + result.isSuccess());
+	}
 
-    @Override
-    public void setPresenter(LoginContract.Presenter presenter) {
-        if (mPresenter == null) {
-            mPresenter = presenter;
-        }
-    }
+	@Override
+	public void setPresenter(LoginContract.Presenter presenter) {
+		if (mPresenter == null) {
+			mPresenter = presenter;
+		}
+	}
 
 
-    @Override
-    public void onDestroyView() {
-        super.onDestroyView();
+	@Override
+	public void onDestroyView() {
+		super.onDestroyView();
 //        unbinder.unbind();
-    }
+	}
 
-    @OnClick(R.id.bt_ok)
-    public void onViewClicked() {
-        Observable<CharSequence> ObservableName = RxTextView.textChanges(name);
-        Observable<CharSequence> ObservablePassword = RxTextView.textChanges(pwd);
+	@OnClick(R.id.bt_ok_test01)
+	public void onViewClicked() {
+		LogTools.i(TAG, "onViewClick: ");
+		mAuth.createUserWithEmailAndPassword(name.getText().toString(), pwd.getText().toString())
+			.addOnCompleteListener(getActivity(), new OnCompleteListener<AuthResult>() {
+				@Override
+				public void onComplete(@NonNull Task<AuthResult> task) {
+					LogTools.i(TAG, "onComplete: " + task.isSuccessful());
+					if (task.isSuccessful())
+						LogTools.d(TAG, "name=: " + mAuth.getCurrentUser().toString());
+				}
+			});
+		
+//		Observable<CharSequence> ObservableName = RxTextView.textChanges(name);
+//		Observable<CharSequence> ObservablePassword = RxTextView.textChanges(pwd);
+//
+//		Observable.combineLatest(ObservableName, ObservablePassword
+//			, (phone, password) -> isUsrValid(phone.toString()) && isPasswordValid(password.toString()))
+//			.subscribe(btnLogin::setEnabled);
+//		RxView.clicks(btnLogin)
+//			.throttleFirst(1, TimeUnit.SECONDS)
+//			.subscribeOn(AndroidSchedulers.mainThread())
+//			.subscribe(v -> {
+//				String username = name.getText().toString();
+//				String password = pwd.getText().toString();
+//				mPresenter.login(username, password);
+//			});
+	}
 
-        Observable.combineLatest(ObservableName, ObservablePassword
-                , (phone, password) -> isUsrValid(phone.toString()) && isPasswordValid(password.toString()))
-                .subscribe(btnLogin::setEnabled);
-        RxView.clicks(btnLogin)
-                .throttleFirst(1, TimeUnit.SECONDS)
-                .subscribeOn(AndroidSchedulers.mainThread())
-                .subscribe(v -> {
-                    String username = name.getText().toString();
-                    String password = pwd.getText().toString();
-                    mPresenter.login(username, password);
-                });
-    }
+	@Override
+	public void onDestroy() {
+		super.onDestroy();
+		FirebaseAuth.getInstance().signOut();
+	}
 }
