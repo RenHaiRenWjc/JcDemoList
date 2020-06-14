4cb0fbd96dfffcb7b536f631251d1296fe6d5a5c
jcChen <wjchenrhr@qq.com>
2020-06-14 22:53:36 +0800

    jetpack navigation

--- app/src/main/java/com/wjc/jcdemolist/demo/jetpack/MainFragment.java
+++ app/src/main/java/com/wjc/jcdemolist/demo/jetpack/MainFragment.java
@@ -1,27 +1,22 @@
-package com.wjc.jcdemolist.demo.jetpack.ui.login;
+package com.wjc.jcdemolist.demo.jetpack;
 
 import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import androidx.fragment.app.Fragment;
-import androidx.navigation.fragment.NavHostFragment;
-
 import com.wjc.jcdemolist.R;
-import com.wjc.jcdemolist.Utils.LogTools;
-
-import butterknife.ButterKnife;
-import butterknife.OnClick;
-import butterknife.Unbinder;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link WelcomeFragment#newInstance} factory method to
+ * Use the {@link MainFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class WelcomeFragment extends Fragment {
-	private static final String TAG = "WelcomeFragment";
+public class MainFragment extends Fragment {
+
 	// TODO: Rename parameter arguments, choose names that match
 	// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 	private static final String ARG_PARAM1 = "param1";
@@ -30,9 +25,8 @@ public class WelcomeFragment extends Fragment {
 	// TODO: Rename and change types of parameters
 	private String mParam1;
 	private String mParam2;
-	private Unbinder mUnbinder;
 
-	public WelcomeFragment() {
+	public MainFragment() {
 		// Required empty public constructor
 	}
 
@@ -42,11 +36,11 @@ public class WelcomeFragment extends Fragment {
 	 *
 	 * @param param1 Parameter 1.
 	 * @param param2 Parameter 2.
-	 * @return A new instance of fragment WelcomeFragment.
+	 * @return A new instance of fragment MainFragment.
 	 */
 	// TODO: Rename and change types and number of parameters
-	public static WelcomeFragment newInstance(String param1, String param2) {
-		WelcomeFragment fragment = new WelcomeFragment();
+	public static MainFragment newInstance(String param1, String param2) {
+		MainFragment fragment = new MainFragment();
 		Bundle args = new Bundle();
 		args.putString(ARG_PARAM1, param1);
 		args.putString(ARG_PARAM2, param2);
@@ -66,24 +60,7 @@ public class WelcomeFragment extends Fragment {
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 							 Bundle savedInstanceState) {
-		View mRootView = inflater.inflate(R.layout.fragment_welcome, container, false);
-		mUnbinder = ButterKnife.bind(this, mRootView);
-		return mRootView;
-	}
-
-	@OnClick({R.id.bt_register, R.id.bt_login})
-	public void onViewClicked(View view) {
-		switch (view.getId()) {
-			case R.id.bt_register:
-				LogTools.i(TAG, ":bt_register ");
-//				NavHostFragment.findNavController(this)
-//					.navigate(R.id.action_welcomeFragment_to_registerFragment);
-				break;
-			case R.id.bt_login:
-				LogTools.i(TAG, ": bt_login");
-//				NavHostFragment.findNavController(this)
-//					.navigate(R.id.action_welcomeFragment_to_jcLoginFragment);
-				break;
-		}
+		// Inflate the layout for this fragment
+		return inflater.inflate(R.layout.fragment_main, container, false);
 	}
 }
